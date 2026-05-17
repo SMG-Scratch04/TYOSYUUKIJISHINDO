0d7574b139e6b3048860340488ed662844d22737
SMG-Scratch04 <kira.hasegawa12@gmail.com>
2026-05-17 20:03:01 +0900

    Update aa.js

--- aa.js
+++ aa.js
@@ -4,6 +4,11 @@ const url = "https://www.jma.go.jp/bosai/ltpgm/data/list.json";
 fetch(proxy + url)
   .then(res => res.json())
   .then(data => {
+
+    // 生 JSON を表示
+    document.getElementById("raw").textContent = JSON.stringify(data, null, 2);
+
+    // 整形表示
     const out = document.getElementById("output");
     out.innerHTML = "";
 
@@ -18,5 +23,6 @@ fetch(proxy + url)
   })
   .catch(err => {
     console.error(err);
+    document.getElementById("raw").textContent = "読み込みエラー";
     document.getElementById("output").textContent = "読み込みエラー";
   });
