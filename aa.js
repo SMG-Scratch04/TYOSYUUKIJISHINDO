cc9c1513cf4991e90833ffa3d9f3681eb2fef202
SMG-Scratch04 <kira.hasegawa12@gmail.com>
2026-05-17 20:40:32 +0900

    Update aa.js

--- aa.js
+++ aa.js
@@ -15,9 +15,7 @@ fetch(proxy + encodeURIComponent(url))
 function syntaxHighlight(json) {
   json = json.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
   return json.replace(
-    /("(\\u[a-zA-Z0-9]{4}|\
-
-\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
     function (match) {
       let cls = "number";
       if (/^"/.test(match)) {
